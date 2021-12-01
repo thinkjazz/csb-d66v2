f8652c83282fd1db11c0e6ac46d830a766c67cfc
Code Sandbox <hello@codesandbox.io>
2021-12-01 22:54:13 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e4927acf1719b2e7d3ba6c0ec23f81f8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ec471a9c5e4775e05bd5761463a435",
+    "url": "https://.github.io/csb-d66v2/index.html"
+  },
+  {
+    "revision": "c9e66fdfefc344bcaf4a",
+    "url": "https://.github.io/csb-d66v2/static/css/main.bf0b79b5.chunk.css"
+  },
+  {
+    "revision": "a75bdf9be599096ef24b",
+    "url": "https://.github.io/csb-d66v2/static/js/2.cc938875.chunk.js"
+  },
+  {
+    "revision": "23b26446db404c4cd1d2ae5ba4f80a38",
+    "url": "https://.github.io/csb-d66v2/static/js/2.cc938875.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9e66fdfefc344bcaf4a",
+    "url": "https://.github.io/csb-d66v2/static/js/main.b1340662.chunk.js"
+  },
+  {
+    "revision": "c2ee665f7d716fd81cda",
+    "url": "https://.github.io/csb-d66v2/static/js/runtime-main.dc1ec378.js"
+  },
+  {
+    "revision": "eff8d38422a968540b517037810e8b43",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-1.eff8d384.jpg"
+  },
+  {
+    "revision": "1033d7e2f7cc4b4e6f47aa3b42744752",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-10.1033d7e2.jpg"
+  },
+  {
+    "revision": "6e4bd9700626b927735c6f64b2cb8e1b",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-11.6e4bd970.jpg"
+  },
+  {
+    "revision": "834ea713922f43d3fe61cd5ca36e0355",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-12.834ea713.jpg"
+  },
+  {
+    "revision": "ea8e51fb733ec168844d6a035cd39146",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-13.ea8e51fb.jpg"
+  },
+  {
+    "revision": "411aa813c21aa06b90a3d8c52ca53e5e",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-14.411aa813.jpg"
+  },
+  {
+    "revision": "458bc44f229211ab7520d667e588222a",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-2.458bc44f.jpg"
+  },
+  {
+    "revision": "d5391c64bf301b9e7bd868c6535d49c9",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-3.d5391c64.jpg"
+  },
+  {
+    "revision": "743d7512850785f97f3a9cac93912f1f",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-4.743d7512.jpg"
+  },
+  {
+    "revision": "72e6e7b718991405de410a48248cd4ea",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-5.72e6e7b7.jpg"
+  },
+  {
+    "revision": "55592ea03e52656fa09e43e60e2856b8",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-6.55592ea0.jpg"
+  },
+  {
+    "revision": "51bd886105bf3a62c9676b618766d426",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-7.51bd8861.jpg"
+  },
+  {
+    "revision": "82173777fe6899216d694ed5e3c76c15",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-8.82173777.jpg"
+  },
+  {
+    "revision": "e0195cf8ab7ea2ddeaf9a14f2e0f4358",
+    "url": "https://.github.io/csb-d66v2/static/media/crop-9.e0195cf8.jpg"
+  }
+]);
